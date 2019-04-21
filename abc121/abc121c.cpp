acfa927b65a22781fdb2c23dbb01f0c4ec7785ad
acannie <acannie0530@gmail.com>
2019-04-21 20:52:16 +0900

    edited abc121c.cpp

--- abc121/abc121c.cpp
+++ abc121/abc121c.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <stdio.h>
+#include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 int main()
@@ -8,32 +11,65 @@ int main()
 
     cin >> N >> M;
 
-    int A[N], B[N];
+    //firstは金額、secondは個数
+    vector<pair<int, int>> pairs(N);
 
     for (int i = 0; i < N; i++)
     {
-        cin >> A[i] >> B[i];
+        int a, b;
+        cin >> a >> b;
+        pairs[i] = make_pair(a, b);
     }
 
-    sort(A, A + N);
+    sort(pairs.begin(), pairs.end());
 
-    for(int i = 0; i < N; i++)
-    {
-        B[i] = B[]
-    }
-    
+    long long ans = 0;
 
-    int ans = 0;
+    /*
     int store = 0;
 
-    for (int i = 0; i < M; i++)
+    for (int i = 0; i < M; i++) //iは買ったドリンクの本数
     {
-        if (B[store] == 0)
+        pair<int, int> pair = pairs[store];
+
+        int price = pair.first;
+        int num = pair.second;
+
+        ans = ans + (long long)price;
+
+        num--;
+        pairs[store] = make_pair(price, num);
+
+        if (num == 0)
         {
             store++;
         }
-        ans = ans + A[store];
-        B[store]--;
+    }
+    */
+    int i = 0;
+    while ((M > 0) && (i < N))
+    {
+        pair<int, int> pair = pairs[i];
+
+        int price = pair.first;
+        int num = pair.second;
+
+        /*if (M >= num)
+        {
+            ans = ans + (long long)price * (long long)num;
+            M = M - num;
+        }
+        else
+        {
+            ans = ans + (long long)price * (long long)M;
+            M = 0;
+        }
+        */
+
+        ans = ans + (long long)price * (long long)min(num, M);
+        M = M - min(num, M);
+
+        i++;
     }
 
     cout << ans << endl;
