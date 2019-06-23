e18085e619e7222dd9912a6aa97375f8deb757f5
acannie <acannie0530@gmail.com>
2019-06-23 16:07:18 +0900

    added M-solution, abd127-131, agc034, diverta2, template.cpp

---
+++ abc130/abc130e.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int N;
+	
+	cin >> N;
+	
+	int a[N];
+	string S;
+	
+	for(int i = 1; i <= N; i++)
+	{
+		cin >> a[i];
+	}
+	
+	int ans;
+	
+	cout << ans << endl;
+	
+	return 0;
+}
