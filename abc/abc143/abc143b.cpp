d966a055f220bb1c600845ccec2f04c86b0b494c
acannie <acannie0530@gmail.com>
2019-10-24 22:44:24 +0900

    added abc143 programs.

--- abc/abc143/abc143b.cpp
+++ abc/abc143/abc143b.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int main()
 {
-	int N;
-	string S;
+	long long int N;
 
 	cin >> N;
-	
-	int a[N];
- 	
- 	for(int i = 0; i < N; i++)
- 	{
- 		cin >> a[i];
- 	}
-  	
-  	int ans;
-  	
-   	cout << ans << endl;
-  	
- 	return 0;
- }
+
+	long long int d[N];
+
+	for (long long int i = 0; i < N; i++)
+	{
+		cin >> d[i];
+	}
+
+	long long int ans = 0;
+
+	for (long long int i = 0; i < N; i++)
+	{
+		for (long long int j = i + 1; j < N; j++)
+		{
+			ans += d[i] * d[j];
+		}
+	}
+
+	cout << ans << endl;
+
+	return 0;
+}
---
+++ agc/agc035/abc143b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int N;
+	string S;
+
+	cin >> N;
+	
+	int a[N];
+ 	
+ 	for(int i = 0; i < N; i++)
+ 	{
+ 		cin >> a[i];
+ 	}
+  	
+  	int ans;
+  	
+   	cout << ans << endl;
+  	
+ 	return 0;
+ }
