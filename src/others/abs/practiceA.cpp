4e1e1779efe947a5883c9a923814a58ee26962fd
acannie <acannie0530@gmail.com>
2020-03-01 17:52:25 +0900

    cmake

---
+++ src/others/abs/practiceA.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void solve(std::istream& ist, std::ostream& ost)
+{
+    int a, b, c;
+    string s;
+    
+    //hundle input
+    ist >> a;
+
+    ist >> b >> c;
+
+    ist >> s;
+    
+    int sum = a + b + c;
+
+    ost << sum << " " << s << endl;
+}
+
+#ifndef DEBUG
+int main()
+{
+    solve(std::cin, std::cout);
+    return 0;
+}
+#endif
