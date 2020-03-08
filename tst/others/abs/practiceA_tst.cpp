8703c83359e0c47dc0b5019134354d2e8ea406c8
fujigon <dec12.kaz@gmail.com>
2020-03-08 17:22:48 +0900

    fix compile

--- tst/others/abs/practiceA_tst.cpp
+++ tst/others/abs/practiceA_tst.cpp
@@ -1,6 +1,8 @@
 #include "solve.h"
 #include "gtest/gtest.h"
 
+using namespace std::literals::string_literals;
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
