c3d700d4bd7a7c0410d699e0a448359b57ff9ff1
acannie <acannie0530@gmail.com>
2020-03-09 17:12:06 +0900

    added abc158d.

--- tst/abc/abc158/abc158d_tst.cpp
+++ tst/abc/abc158/abc158d_tst.cpp
@@ -3,15 +3,16 @@
 
 using namespace std::literals::string_literals;
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
-TEST(Test1, case1) {
-
-  std::string in = "1 2 3 6"s;
-  std::string out = "6 6"s;
+TEST(Test1, case1)
+{
+  std::string in = "a\n 4\n 2 1 p\n 1\n 2 2 c\n 1 "s;
+  std::string out = "cpa"s;
 
   std::ostringstream ost;
   std::istringstream ist(in + "\n");
@@ -22,3 +23,32 @@ TEST(Test1, case1) {
   EXPECT_EQ(ostr, out + "\n");
 }
 
+TEST(Test1, case2)
+{
+
+  std::string in = "a 6 2 2 a 2 1 b 1 2 2 c 1 1 "s;
+  std::string out = "aabc"s;
+
+  std::ostringstream ost;
+  std::istringstream ist(in + "\n");
+
+  solve(ist, ost);
+  std::string ostr = ost.str();
+
+  EXPECT_EQ(ostr, out + "\n");
+}
+
+TEST(Test1, case3)
+{
+
+  std::string in = "y 1 2 1 x"s;
+  std::string out = "xy"s;
+
+  std::ostringstream ost;
+  std::istringstream ist(in + "\n");
+
+  solve(ist, ost);
+  std::string ostr = ost.str();
+
+  EXPECT_EQ(ostr, out + "\n");
+}
